0f77db2ee56c8a689b9f7d63b57830335d2eeb41
urvil nileshbhai jivani <f20170943@goa.bits-pilani.ac.in>
2020-06-14 01:16:05 +0530

    Updates

---
+++ precache-manifest.aff74884f5fffd1ccfa1c603d5af7471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643c58e4ec4c7e1a79b637a75b5c306e",
+    "url": "/Simple-React-App/index.html"
+  },
+  {
+    "revision": "7a29c9aff34103e20c89",
+    "url": "/Simple-React-App/static/css/main.dac855d8.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Simple-React-App/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Simple-React-App/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a29c9aff34103e20c89",
+    "url": "/Simple-React-App/static/js/main.ed37066f.chunk.js"
+  },
+  {
+    "revision": "a9b769bfcf39921eeda0",
+    "url": "/Simple-React-App/static/js/runtime-main.544db507.js"
+  }
+]);
